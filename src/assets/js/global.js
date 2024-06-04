4837375b98254252e9593f6d0d9b3535ee8f94ed
Koki Ueda <60583832+kokitanshio@users.noreply.github.com>
2024-06-05 02:08:50 +0900

    [update]_202406変数と基本設定の変更

--- src/assets/js/global.js
+++ src/assets/js/global.js
@@ -1,3 +1,23 @@
 export const globalFunc = () => {
-  console.log("globalFunc");
+  // 375px未満はVPを固定
+  !(function () {
+    const viewport = document.querySelector('meta[name="viewport"]');
+    function switchViewport() {
+      const value = window.outerWidth > 375 ? "width=device-width,initial-scale=1" : "width=375";
+      if (viewport.getAttribute("content") !== value) {
+        viewport.setAttribute("content", value);
+      }
+    }
+    addEventListener("resize", switchViewport, false);
+    switchViewport();
+  })();
+
+  // 外部リンクを新規タブで開く
+  document.querySelectorAll('a[href^="http"]').forEach(function (link) {
+    if (!link.href.includes(location.hostname)) {
+      link.setAttribute("target", "_blank");
+      link.setAttribute("rel", "noopener noreferrer");
+      link.classList.add("blank");
+    }
+  });
 };
